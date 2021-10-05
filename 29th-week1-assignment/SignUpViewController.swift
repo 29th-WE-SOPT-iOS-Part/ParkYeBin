85ac5a7077487bbef3af36b27656fdd5c3c09042
beansbin <beans_bin@naver.com>
2021-10-05 17:40:59 +0900

    ✨[FEAT] 회원가입 화면에서 적은 이름을 확인 화면에서 띄우기

--- 29th-week1-assignment/SignUpViewController.swift
+++ 29th-week1-assignment/SignUpViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @IBAction func touchUpToGoComplete(_ sender: Any) {
-        guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "CompleteViewController") else { return }
+        guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "CompleteViewController") as? CompleteViewController else { return }
         
+        nextVC.name = self.nameTextField.text
         nextVC.modalPresentationStyle = .fullScreen
         self.present(nextVC, animated: false, completion: nil)
     }
