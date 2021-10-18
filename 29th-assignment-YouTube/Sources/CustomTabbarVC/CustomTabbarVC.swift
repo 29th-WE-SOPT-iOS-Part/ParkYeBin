5e30c0ec39aafe608f1cfd1ce66cf432617518c4
Park Yebin <talk2769@naver.com>
2021-10-19 00:36:10 +0900

    ➕[ADD] CustomTabbarVC 생성

---
+++ 29th-assignment-YouTube/Sources/CustomTabbarVC/CustomTabbarVC.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabbarVC.swift
+//  29th-assignmnet-YouTube
+//
+//  Created by 박예빈 on 2021/10/19.
+//
+
+import UIKit
+
+class CustomTabbarVC: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
