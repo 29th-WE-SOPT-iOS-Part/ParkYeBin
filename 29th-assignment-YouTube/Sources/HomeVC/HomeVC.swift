0f8a3c983e195c9e48d85c27bab30d85099bdab5
beansBin <beans_bin@naver.com>
2021-10-29 02:21:52 +0900

    ✨[FEAT] 홈화면 CollectionView DataSource 구현

--- 29th-assignment-YouTube/Sources/HomeVC/HomeVC.swift
+++ 29th-assignment-YouTube/Sources/HomeVC/HomeVC.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    @IBOutlet weak var storyCollectionView: UICollectionView!
+    
+    let storyLabels: [String] = ["iOSPart", "AndroidPart", "ServerPart", "WebPart", "DesignPart", "PlanPart"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension HomeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return storyLabels.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
 
+extension HomeVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 72, height: 104)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets.zero
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 0
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 0
+        }
 }
---
+++ 29th-assignment-YouTube/Sources/HomeVC/StoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StoryCollectionViewCell.swift
+//  29th-assignmnet-YouTube
+//
+//  Created by 박예빈 on 2021/10/29.
+//
+
+import UIKit
+
+class StoryCollectionViewCell: UICollectionViewCell {
+    
+}
