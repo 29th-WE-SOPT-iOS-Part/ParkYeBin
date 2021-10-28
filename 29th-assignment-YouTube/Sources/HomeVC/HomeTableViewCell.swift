acca87720dba9460c67f99ada4db9d10c49ab9f8
beansBin <beans_bin@naver.com>
2021-10-29 03:45:13 +0900

    ✨[FEAT] HomeTableViewCell.xib 구성 및 AutoLayout

---
+++ 29th-assignment-YouTube/Sources/HomeVC/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  29th-assignmnet-YouTube
+//
+//  Created by 박예빈 on 2021/10/29.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
